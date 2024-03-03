7eba772f7a2638e4db857bb7e0e2e1dfccd61a56
Dutus244 <duynguyen24th@gmail.com>
2024-03-03 22:26:20 +0700

    ND splash and welcome page

---
+++ lib/pages/welcome/welcome.dart
@@ -0,0 +1,163 @@
+import 'package:dots_indicator/dots_indicator.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../common/values/colors.dart';
+import 'bloc/welcome_blocs.dart';
+import 'bloc/welcome_events.dart';
+import 'bloc/welcome_states.dart';
+
+class Welcome extends StatefulWidget {
+  const Welcome({super.key});
+
+  @override
+  State<Welcome> createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<Welcome> {
+  PageController pageController = PageController(initialPage: 0);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: AppColors.primaryBackground,
+      child: Scaffold(body: BlocBuilder<WelcomeBloc, WelcomeState>(
+        builder: (context, state) {
+          return Container(
+            margin: EdgeInsets.only(top: 34.h),
+            width: 375.w,
+            child: Stack(
+              alignment: Alignment.topCenter,
+              children: [
+                PageView(
+                  controller: pageController,
+                  onPageChanged: (index) {
+                    state.page = index;
+                    BlocProvider.of<WelcomeBloc>(context).add(WelcomeEvent());
+                  },
+                  children: [
+                    _page(
+                        1,
+                        context,
+                        "Next",
+                        "Welcome back!",
+                        "Explore new features and opportunities in the alumni community.",
+                        "assets/images/welcome1.png"),
+                    _page(
+                        2,
+                        context,
+                        "Next",
+                        "Discover new apps",
+                        "Learn how our app can help you stay connected and thrive after college.",
+                        "assets/images/welcome2.png"),
+                    _page(
+                        3,
+                        context,
+                        "Get started",
+                        "Start your journey",
+                        "Log in or register to experience the full app features and join the alumni community.",
+                        "assets/images/welcome3.png"),
+                  ],
+                ),
+                Positioned(
+                    bottom: 100.h,
+                    child: DotsIndicator(
+                      position: state.page,
+                      dotsCount: 3,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      decorator: DotsDecorator(
+                          color: AppColors.primaryThirdElementText,
+                          activeColor: AppColors.primaryElement,
+                          size: const Size.square(8.0),
+                          activeSize: const Size(18.0, 8.0),
+                          activeShape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5.0),
+                          )),
+                    ))
+              ],
+            ),
+          );
+        },
+      )),
+    );
+  }
+
+  Widget _page(int index, BuildContext context, String buttonName, String title,
+      String subTitle, String imagePath) {
+    return Column(
+      children: [
+        SizedBox(
+          width: 345.w,
+          height: 345.w,
+          child: Image.asset(
+            imagePath,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Container(
+          child: Text(
+            "${title}",
+            style: TextStyle(
+                color: AppColors.primaryText,
+                fontSize: 24.sp,
+                fontWeight: FontWeight.normal),
+          ),
+        ),
+        Container(
+          width: 375.w,
+          padding: EdgeInsets.only(left: 30.w, right: 30.w),
+          child: Text(
+            "${subTitle}",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                color: AppColors.primaryFourthElementText,
+                fontSize: 14.sp,
+                fontWeight: FontWeight.normal),
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            if (index < 3) {
+              pageController.animateToPage(
+                index,
+                duration: const Duration(milliseconds: 500),
+                curve: Curves.decelerate,
+              );
+            } else {
+              // Navigator.of(context).push(MaterialPageRoute(builder: (context)=>MyHomePage()));
+              // Navigator.of(context)
+              //     .pushNamedAndRemoveUntil("signIn", (route) => false);
+            }
+          },
+          child: Container(
+            margin: EdgeInsets.only(top: 100.h, left: 25.w, right: 25.w),
+            width: 325.w,
+            height: 50.h,
+            decoration: BoxDecoration(
+                color: AppColors.primaryElement,
+                borderRadius: BorderRadius.all(Radius.circular(15.w)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 10,
+                    blurRadius: 20,
+                    offset: Offset(0, 1),
+                  )
+                ]),
+            child: Center(
+              child: Text(
+                "${buttonName}",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16.sp,
+                    fontWeight: FontWeight.normal),
+              ),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
