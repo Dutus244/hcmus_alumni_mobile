e46c4d4ee020069ee852a48664f456ee747dd726
Dutus244 <duynguyen24th@gmail.com>
2024-03-04 23:05:24 +0700

    ND signin page ui

---
+++ lib/pages/sign_in/bloc/sign_in_events.dart
@@ -0,0 +1,18 @@
+abstract class SignInEvent {
+  const SignInEvent();
+}
+
+class EmailEvent extends SignInEvent {
+  final String email;
+  const EmailEvent(this.email);
+}
+
+class PasswordEvent extends SignInEvent {
+  final String password;
+  const PasswordEvent(this.password);
+}
+
+class RememberLoginEvent extends SignInEvent {
+  final bool rememberLogin;
+  const RememberLoginEvent(this.rememberLogin);
+}
---
+++ lib/pages/sign_in/sign_in_controller.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../common/widgets/flutter_toast.dart';
+import 'bloc/sign_in_blocs.dart';
+
+class SignInController {
+  final BuildContext context;
+
+  const SignInController({required this.context});
+
+  Future<void> handleSignIn(String type) async {
+    try {
+      if (type == "email") {
+        // BlocProvider.of<SignInBloc>(context).state
+        final state = context.read<SignInBloc>().state;
+        String emailAddress = state.email;
+        String password = state.password;
+        if (emailAddress.isEmpty) {
+          toastInfo(msg: "Bạn phải điền email");
+          return;
+        }
+        if (password.isEmpty) {
+          toastInfo(msg: "Bạn phải điền password");
+          return;
+        }
+      }
+    } catch (e) {}
+  }
+}
