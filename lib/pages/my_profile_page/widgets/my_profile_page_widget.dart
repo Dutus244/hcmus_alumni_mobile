1f905f655f8e274cfc41f0784b1f987669453249
Dutus244 <duynguyen24th@gmail.com>
2024-07-30 18:33:27 +0700

    Update team

--- lib/pages/my_profile_page/widgets/my_profile_page_widget.dart
+++ lib/pages/my_profile_page/widgets/my_profile_page_widget.dart
@@ -600,12 +600,12 @@ Widget listFriend(BuildContext context) {
                             fit: BoxFit.cover,
                           ),
                         ),
-                        height: 100.h,
-                        width: 100.h,
+                        height: 95.h,
+                        width: 95.h,
                       ),
                       Container(
                         margin: EdgeInsets.only(top: 10.h, bottom: 10.h),
-                        width: 100.w,
+                        width: 95.w,
                         height: 40.h,
                         child: Text(
                           BlocProvider.of<MyProfilePageBloc>(context)
@@ -671,12 +671,12 @@ Widget listFriend(BuildContext context) {
                               fit: BoxFit.cover,
                             ),
                           ),
-                          height: 100.h,
-                          width: 100.h,
+                          height: 95.h,
+                          width: 95.h,
                         ),
                         Container(
                           margin: EdgeInsets.only(top: 10.h, bottom: 10.h),
-                          width: 100.w,
+                          width: 95.w,
                           height: 40.h,
                           child: Text(
                             BlocProvider.of<MyProfilePageBloc>(context)
