d886ba1ebf1ef80cb34769ceb271ba7702203628
Dutus244 <duynguyen24th@gmail.com>
2024-07-31 18:43:54 +0700

    Update gitgnore

--- lib/pages/option_page/option_page_controller.dart
+++ lib/pages/option_page/option_page_controller.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_translate/flutter_translate.dart';
 import 'package:http/http.dart' as http;
 
 import '../../common/services/firebase_service.dart';
@@ -43,26 +45,45 @@ class OptionPageController {
   }
 
   Future<void> handleDeleteUser() async {
-    var token = Global.storageService.getUserAuthToken();
-    var url =
-    Uri.parse('${dotenv.env['API_URL']}/user/self');
-    var headers = <String, String>{
-      'Authorization': 'Bearer $token',
-      'Content-Type': 'application/json',
-    };
-    try {
-      var response = await http.delete(url, headers: headers);
-      print(response.body);
-    } catch (error) {
-      print(error);
+    final shouldDelete = await showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Xoá tài khoản'),
+        content: Text('Bạn có muốn xoá tài khoản của mình?'),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context, false),
+            child: Text(translate('cancel')),
+          ),
+          TextButton(
+            onPressed: () => Navigator.pop(context, true),
+            child: Text(translate('delete')),
+          ),
+        ],
+      ),
+    );
+    if (shouldDelete != null && shouldDelete) {
+      var token = Global.storageService.getUserAuthToken();
+      var url =
+      Uri.parse('${dotenv.env['API_URL']}/user/self');
+      var headers = <String, String>{
+        'Authorization': 'Bearer $token',
+        'Content-Type': 'application/json',
+      };
+      try {
+        var response = await http.delete(url, headers: headers);
+        print(response.body);
+      } catch (error) {
+        print(error);
+      }
+      Global.storageService.setString(AppConstants.USER_AUTH_TOKEN, '');
+      Global.storageService
+          .setBool(AppConstants.USER_REMEMBER_LOGIN, false);
+      Global.storageService.setString(AppConstants.USER_EMAIL, '');
+      Global.storageService.setString(AppConstants.USER_PASSWORD, '');
+      socketService.disconnect();
+      Navigator.of(context)
+          .pushNamedAndRemoveUntil("/signIn", (route) => false);
     }
-    Global.storageService.setString(AppConstants.USER_AUTH_TOKEN, '');
-    Global.storageService
-        .setBool(AppConstants.USER_REMEMBER_LOGIN, false);
-    Global.storageService.setString(AppConstants.USER_EMAIL, '');
-    Global.storageService.setString(AppConstants.USER_PASSWORD, '');
-    socketService.disconnect();
-    Navigator.of(context)
-        .pushNamedAndRemoveUntil("/signIn", (route) => false);
   }
 }
